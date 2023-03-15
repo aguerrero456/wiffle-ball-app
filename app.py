ed56a9313e68f6e3020c4866a1454b5bf45d7054
Alfredo Guerrero <95371639+aguerrero456@users.noreply.github.com>
2023-03-15 02:11:56 +0000

    Added HTML, bootstrap, dynamic data, and API route

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+PLAYERS = [{
+  'id': 1,
+  'Fname': 'Aaron',
+  'Nname': 'Atomic Bomb',
+  'B/T': 'R/R',
+  'Spitch': 'Twice Tombstone',
+  'TC': 'true'
+}, {
+  'id': 2,
+  'Fname': 'Fred',
+  'B/T': 'R/R',
+  'TC': 'false'
+}]
 
 
 @app.route("/")
 def hello_world():
-  return "Hello, Alfredo"
+  return render_template('home.html', players=PLAYERS)
+
+@app.route("/api/players")
+def list_players():
+  return jsonify(PLAYERS)
 
 
 if __name__ == "__main__":
