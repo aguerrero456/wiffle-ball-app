02188cc91d139cb7952833a89220b5b560faf3e5
Alfredo Guerrero <95371639+aguerrero456@users.noreply.github.com>
2023-03-21 19:21:39 +0000

    Updated Navbar

---
+++ database.py
@@ -0,0 +1,3 @@
+from sqlalchemy import create_engine
+
+engine = create_engine("mysql+pymysql://5jvxqwm3a8dxt2kpnq2w:pscale_pw_mFAJD6jf4eVGyGnPCrUAadhUrKukDlOiPrRXV6B6CjQ@us-west.connect.psdb.cloud/wiffleball?charset=utf8mb4")
