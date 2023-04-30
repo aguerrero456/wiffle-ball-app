39d7c844d25bb3db2a2a5565a1e8d490249809ec
aguerrero456 <95371639+aguerrero456@users.noreply.github.com>
2023-04-30 08:10:33 +0000

    Removed sensitive information.

--- database.py
+++ database.py
@@ -1,14 +1,13 @@
 from sqlalchemy import create_engine, text
+import os
 
-db_connection_string = "mysql+pymysql://5jvxqwm3a8dxt2kpnq2w:pscale_pw_mFAJD6jf4eVGyGnPCrUAadhUrKukDlOiPrRXV6B6CjQ@us-west.connect.psdb.cloud/wiffleball?charset=utf8mb4"
+db_connection_string = os.environ['DB_CONNECTION_STRING']
 
-engine = create_engine(
-  db_connection_string,
-  connect_args={"ssl": {
-    "ssl_ca": "/etc/ssl/cert.pem"
-    }
-  }
-)
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
 
 def load_player_info():
   with engine.connect() as conn:
@@ -17,4 +16,3 @@ def load_player_info():
     for row in result.all():
       player_info.append(dict(row._mapping))
     return player_info
-
