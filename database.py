8de763fa7c1700e5a82375a5328b76acef8d8d99
aguerrero456 <95371639+aguerrero456@users.noreply.github.com>
2023-04-30 08:29:55 +0000

    Added database.py

---
+++ database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine, text
+import os
+
+db_connection_string = os.environ['DB_CONNECTION_STRING']
+
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
+
+def load_player_info():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from Players"))
+    player_info = []
+    for row in result.all():
+      player_info.append(dict(row._mapping))
+    return player_info
+  
